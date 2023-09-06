5a26e54457d4afbbea2de07f4b26cd3f37ce2dc1
Fortune Ebarim <fortuneebarim@gmail.com>
2023-09-06 03:02:14 +0000

    update

--- anchor/anchor/settings.py
+++ anchor/anchor/settings.py
@@ -153,6 +153,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 
 # Default primary key field type
--- anchor/anchor/urls.py
+++ anchor/anchor/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 from .views import CustomLoginView, CustomLogoutView, RegistrationView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -35,7 +37,8 @@ urlpatterns = [
     path('accounts/register/', RegistrationView.as_view(), name='register'),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
     
