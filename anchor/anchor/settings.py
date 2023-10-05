511d20899d19ff2c21847bfcabe5e0e5d736a8c5
ebarimjesus <130609758+ebarimjesus@users.noreply.github.com>
2023-10-05 03:31:26 +0000

    update

--- anchor/anchor/settings.py
+++ anchor/anchor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x7t(e2sofl%pexf)8$0(*d0z!q9*ikp$ej=xkwt9&2w#(1#s_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['anchor-bptz.onrender.com', 'localhost', 'zingypay.com']
 
